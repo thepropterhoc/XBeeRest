2782bebca3dbf8eda3cf3a5d6aa5e5401bd35b02
thepropterhoc <vanhooser@ou.edu>
2014-08-04 23:49:16 +0000

    Server is mostly operational

---
+++ rest.py
@@ -0,0 +1,38 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request
+from threading import Thread
+
+app = Flask(__name__)
+
+db = {}
+
+"""
+[{
+	__source_addr : readings,
+	
+}...]
+"""
+
+@app.route('/api/xbee/<string:val_id>/channel/<int:channel_id>', methods = ['GET'])
+def get_val(self, val_id):
+	val = filter(lambda x: x['channel'] == val_id, list(vals))[0]
+	return jsonify(self.val)
+
+@app.route('/api/all/', methods = ['GET'])
+def get_all(self):
+	print vals
+	return jsonify({'vals' : vals})
+
+@app.route('/api/alerts/', methods = ['GET'])
+def get_alerts(self):
+	print alerts
+	return jsonify({'alerts' : alerts})
+	
+@app.route('/api/update', methods = ['POST'])
+def update():
+	frame = request.json
+	db[frame['source_addr_long']] = frame['samples']
+	print db
+	return jsonify(db)
+
+app.run(debug=True)
